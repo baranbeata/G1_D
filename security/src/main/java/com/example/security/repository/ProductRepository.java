8a7c75ef0e42a5eae3e4e685d82e01d41410a131
nati.bienkowska@gmail.com <nati.bienkowska@gmail.com>
2021-05-08 15:56:11 +0200

    G1D-94 Product database backend side - first draft

---
+++ security/src/main/java/com/example/security/repository/ProductRepository.java
@@ -0,0 +1,21 @@
+package com.example.security.repository;
+
+import com.example.security.models.Product;
+import com.example.security.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String name);
+
+    List<Product> findAll();
+    Optional<Product> findById(String id);
+    //Product findBySize(String size);
+   // Boolean existsByUsername(String username);
+
+   // Boolean existsByEmail(String email);
+}
